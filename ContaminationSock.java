12e9584ce5c99031e2c10d77ee179f53f7ed8829
alixayman <alibasiouny6@gmail.com>
2026-03-06 15:50:40 +0200

    Delete ContaminationSock.java

--- ContaminationSock.java
+++
@@ -1,8 +0,0 @@
-package game.engine.cells;
-import game.engine.monsters.*;
-public class ContaminationSock extends TransportCell{
-	
-	public ContaminationSock(String name,int effect){
-		super(name,(effect<0)?effect:0);
-	}
-}
