dc86f0efc1d5cee5dc14786c1cdc4ee07479f3a4
omesorogy <omesorogy@gmail.com>
2026-03-08 19:20:56 +0200

    Delete ConveyorBelt.java

--- ConveyorBelt.java
+++
@@ -1,8 +0,0 @@
-package game.engine.cells;
-import game.engine.monsters.*;
-public class ConveyorBelt extends TransportCell {
-	
-	public ConveyorBelt(String name,int effect){
-		super(name,(effect>0)?effect:0);
-	}
-}
